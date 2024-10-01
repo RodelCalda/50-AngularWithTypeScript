05e4e5c8b5fcf7d4e5c7082d4c3b9d0c5c4170f6
Rodel Calda <rodelcalda2@gmail.com>
2024-10-01 12:48:44 -0400

    Course-List

---
+++ src/app/components/course-list/course-list.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-course-list',
+  templateUrl: './course-list.component.html',
+  styleUrls: ['./course-list.component.css']
+})
+export class CourseListComponent {
+  courseList: string[] = []; 
+  courseName: string = '';   
+
+  
+  addCourse() {
+    if (this.courseName) { 
+      this.courseList.push(this.courseName);
+      this.courseName = ''; 
+    }
+  }
+}
