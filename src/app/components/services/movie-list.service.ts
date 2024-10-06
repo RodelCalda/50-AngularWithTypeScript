d1b8109dbc0e354cc652545ee3170bd4bcc0e3a8
Rodel Calda <rodelcalda2@gmail.com>
2024-10-06 13:27:17 -0400

    enhancement: added Movie-list service

--- src/app/components/services/movie-list.service.ts
+++ src/app/components/services/movie-list.service.ts
@@ -1,28 +1,30 @@
 import { Injectable } from '@angular/core';
+import { Movie } from '../../../interface/movie';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MovieListService {
-  private movies: string[] = ['Inception', 'The Dark Knight', 'Interstellar', 'The Matrix', 'Fight Club'];
+  private movies: Movie[] = [
+    { id: 1, title: 'Inception', director: 'Christopher Nolan', releaseYear: 2010, genre: 'Science Fiction' },
+    { id: 2, title: 'The Shawshank Redemption', director: 'Frank Darabont', releaseYear: 1994, genre: 'Drama' },
+    { id: 3, title: 'The Godfather', director: 'Francis Ford Coppola', releaseYear: 1972, genre: 'Crime' },
+  ];
 
-  constructor() { }
-
-  
-  getMovies(): string[] {
+  getMovies(): Movie[] {
     return this.movies;
   }
 
-  
-  addMovie(movie: string): void {
-    if (movie && !this.movies.includes(movie)) {
-      this.movies.push(movie);
-    }
+  addMovie(movie: Movie): void {
+    this.movies.push(movie);
   }
 
-  
-  removeMovie(movie: string): void {
-    this.movies = this.movies.filter(m => m !== movie);
+  removeMovie(id: number): void {
+    this.movies = this.movies.filter(movie => movie.id !== id);
+  }
+
+  clearMovies(): void {
+    this.movies = [];
   }
 }
 
