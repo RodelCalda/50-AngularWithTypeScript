377f87a8b146fd4c18293ecd419b44fd2146d06f
Rodel Calda <rodelcalda2@gmail.com>
2024-10-09 00:01:52 -0400

    enhancement: added Framework-list service

--- src/app/components/framework-list/framework-list.component.ts
+++ src/app/components/framework-list/framework-list.component.ts
@@ -1,22 +1,44 @@
 import { Component } from '@angular/core';
+import { FrameworkListService } from '../services/framework-list.service';
+import { Framework } from '../../../interface/framework';
 
 @Component({
   selector: 'app-framework-list',
   templateUrl: './framework-list.component.html',
-  styleUrls: ['./framework-list.component.css']
+  styleUrls: ['./framework-list.component.css'],
 })
 export class FrameworkListComponent {
-  
-  frameworksList: string[] = [];
-  
-  frameworkName: string = '';
+  newFramework: Framework = { id: 0, name: '', language: '', description: '' };
+  frameworkList: Framework[] = [];
 
- 
+  constructor(private frameworkService: FrameworkListService) {
+    this.frameworkList = this.frameworkService.getFrameworks(); 
+  }
+
   addFramework() {
-    if (this.frameworkName) { 
-      this.frameworksList.push(this.frameworkName);
-      this.frameworkName = ''; 
+    if (
+      this.newFramework.name.trim() &&
+      this.newFramework.language.trim() &&
+      this.newFramework.description.trim()
+    ) {
+      const newId = this.frameworkList.length
+        ? this.frameworkList[this.frameworkList.length - 1].id + 1
+        : 1;
+      const frameworkToAdd: Framework = { ...this.newFramework, id: newId };
+
+      this.frameworkService.addFramework(frameworkToAdd);
+      this.newFramework = { id: 0, name: '', language: '', description: '' }; 
+      this.frameworkList = this.frameworkService.getFrameworks(); 
     }
   }
-}
 
+  removeFramework(id: number) {
+    this.frameworkService.removeFramework(id);
+    this.frameworkList = this.frameworkService.getFrameworks(); 
+  }
+
+  clearFrameworks() {
+    this.frameworkService.clearFrameworks(); 
+    this.frameworkList = this.frameworkService.getFrameworks(); 
+  }
+}
---
+++ src/interface/framework.ts
@@ -0,0 +1,7 @@
+export interface Framework {
+    id: number;
+    name: string;
+    language: string;
+    description: string;
+  }
+  
